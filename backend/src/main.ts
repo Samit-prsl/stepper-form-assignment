13adab42b4597c7495f54487d96ada79b9039218
Samit Bhattacharjee <sam_prsl@Samits-MacBook-Air.local>
2026-06-07 13:00:51 +0530

    fix/cors-origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,9 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://stepper-form-assignment.vercel.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, 
+    credentials: true,
   });
 
   app.useGlobalPipes(
